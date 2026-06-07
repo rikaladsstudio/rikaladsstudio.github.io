d2ba64178d6356ac46530a093f70d6e81bf04214
rika3000 <rika.rika3000.rika@gmail.com>
2026-06-06 21:47:09 -0600

    attempted to update permissions holicy headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    server: {
+        headers: {
+            "Permissions-Policy": "geolocation=(), camera=(), microphone=(), attribution-reporting=(), run-ad-auction=(), private-state-token-redemption=(self), browsing-topics=()"
+        }
+    },
     devToolbar: {
         enabled: false,
     },
